f704acd4eef29f467c33bc924c744bb1d09d9392
Osagie Omigie <osagieomigie@gmail.com>
2020-08-07 23:48:08 -0600

    Refactored

--- App.js
+++ App.js
@@ -8,10 +8,10 @@ function App() {
         <Text style={styles.header}>
           Here are some boxes of different colours
         </Text>
-        <Text style={styles.firstBox}>Cyan #2aa198</Text>
-        <Text style={styles.secondBox}>Blue #268bd2</Text>
-        <Text style={styles.thirdBox}>Magenta #d33682</Text>
-        <Text style={styles.fourthBox}>Orange #cb4b16</Text>
+        <Text style={[styles.firstBox, styles.Box]}>Cyan #2aa198</Text>
+        <Text style={[styles.secondBox, styles.Box]}>Blue #268bd2</Text>
+        <Text style={[styles.thirdBox, styles.Box]}>Magenta #d33682</Text>
+        <Text style={[styles.fourthBox, styles.Box]}>Orange #cb4b16</Text>
       </View>
     </SafeAreaView>
   );
@@ -27,41 +27,25 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
   },
-  firstBox: {
+  Box: {
     display: "flex",
-    backgroundColor: "#2aa198",
     justifyContent: "center",
     paddingVertical: 5,
     marginHorizontal: 10,
     marginVertical: 3,
     color: "white",
+  },
+  firstBox: {
+    backgroundColor: "#2aa198",
   },
   secondBox: {
-    display: "flex",
-    justifyContent: "center",
-    paddingVertical: 5,
-    marginHorizontal: 10,
-    marginVertical: 3,
     backgroundColor: "#268bd2",
-    color: "white",
   },
   thirdBox: {
-    display: "flex",
-    justifyContent: "center",
-    paddingVertical: 5,
-    marginHorizontal: 10,
-    marginVertical: 3,
     backgroundColor: "#d33682",
-    color: "white",
   },
   fourthBox: {
-    display: "flex",
-    justifyContent: "center",
-    paddingVertical: 5,
-    marginHorizontal: 10,
-    marginVertical: 3,
     backgroundColor: "#cb4b16",
-    color: "white",
   },
   safeArea: {
     flex: 1,
