1a639cc7908b8ff0bf594d05aa9df9091ceb0b3e
Osagie Omigie <osagieomigie@gmail.com>
2020-08-08 17:47:45 -0600

    FlatList

--- App.js
+++ App.js
@@ -1,6 +1,25 @@
 import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { View, Text, SafeAreaView, StyleSheet, FlatList } from "react-native";
 import ColourBox from "./Components/colourBox";
+
+const COLORS = [
+  { colorName: "Base03", hexCode: "#002b36" },
+  { colorName: "Base02", hexCode: "#073642" },
+  { colorName: "Base01", hexCode: "#586e75" },
+  { colorName: "Base00", hexCode: "#657b83" },
+  { colorName: "Base0", hexCode: "#839496" },
+  { colorName: "Base1", hexCode: "#93a1a1" },
+  { colorName: "Base2", hexCode: "#eee8d5" },
+  { colorName: "Base3", hexCode: "#fdf6e3" },
+  { colorName: "Yellow", hexCode: "#b58900" },
+  { colorName: "Orange", hexCode: "#cb4b16" },
+  { colorName: "Red", hexCode: "#dc322f" },
+  { colorName: "Magenta", hexCode: "#d33682" },
+  { colorName: "Violet", hexCode: "#6c71c4" },
+  { colorName: "Blue", hexCode: "#268bd2" },
+  { colorName: "Cyan", hexCode: "#2aa198" },
+  { colorName: "Green", hexCode: "#859900" },
+];
 
 function App() {
   return (
@@ -9,10 +28,13 @@ function App() {
         <Text style={styles.header}>
           Here are some boxes of different colours
         </Text>
-        <ColourBox colourName={"Cyan"} colourHex={"#2aa198"} />
-        <ColourBox colourName={"Blue"} colourHex={"#268bd2"} />
-        <ColourBox colourName={"Magenta"} colourHex={"#d33682"} />
-        <ColourBox colourName={"Orange"} colourHex={"#cb4b16"} />
+        <FlatList
+          data={COLORS}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <ColourBox colourName={item.colorName} colourHex={item.hexCode} />
+          )}
+        />
       </View>
     </SafeAreaView>
   );
